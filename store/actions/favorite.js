24f9f22f331a00243e776d8ca99dfc01534bec20
levosay <ryuuzaki44l@gmail.com>
2022-05-22 11:55:20 +0300

    create: constants action types

--- store/actions/favorite.js
+++ store/actions/favorite.js
@@ -1,7 +1,9 @@
+import {REMOVE_FAVORITE, SET_FAVORITE} from '../../constant/storeTypes'
+
 export const setFavorite = (state) => {
-  return {type: 'SET_FAVORITE', payload: state}
+  return {type: SET_FAVORITE, payload: state}
 }
 
 export const removeFavorite = (state) => {
-  return {type: 'REMOVE_FAVORITE', payload: state}
+  return {type: REMOVE_FAVORITE, payload: state}
 }
--- store/reducers/favoriteReducer.js
+++ store/reducers/favoriteReducer.js
@@ -1,10 +1,12 @@
+import {REMOVE_FAVORITE, SET_FAVORITE} from '../../constant/storeTypes'
+
 const initFavoriteState = []
 
 export const favoriteReducer = (state = initFavoriteState, action) => {
   switch (action.type) {
-    case 'SET_FAVORITE':
+    case SET_FAVORITE:
       return [...state, action.payload]
-    case 'REMOVE_FAVORITE':
+    case REMOVE_FAVORITE:
       return state.filter(item => item.id !== action.payload)
     default:
       return state
